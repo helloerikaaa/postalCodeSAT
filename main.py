fbc5a423351db463dea2236581ff02bd5ea04b04
Erika <esanchez1201@alumno.ipn.mx>
2022-02-17 19:01:33 -0600

    UPDATE: form to calculate zip code

--- main.py
+++ main.py
@@ -5,26 +5,32 @@ import numpy as np
 
 st.title('Encontrar el código postal válido más cercano')
 
-codigo = st.number_input('Código Postal', min_value=None, max_value=None, value=0, step=None,
-                         format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
-localidad = st.number_input('Localidad', min_value=None, max_value=None, value=0, step=None,
-                            format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
-municipio = st.number_input('Municipio', min_value=None, max_value=None, value=0, step=None,
-                            format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
 head_cols = ['codigo_postal', 'estado', 'localidad', 'municipio']
-df_codigos = pd.read_excel('data/catalogo_codigo_postal.xlsx', header=None, names=head_cols)
+df_codigos = pd.read_excel(
+    'data/catalogo_codigo_postal.xlsx', header=None, names=head_cols)
 
 df_codigos = df_codigos.fillna(-1)
 df_codigos = df_codigos.astype(
     {'codigo_postal': int, 'localidad': int, 'municipio': int})
 
-df_codigos = df_codigos[(df_codigos['localidad'] == localidad) & (
-    df_codigos['municipio'] == municipio)]
+with st.form("my_form"):
+    st.write("Por favor llena los campos")
+    codigo = st.number_input('Código Postal', min_value=None, max_value=None, value=0, step=None,
+                             format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+    localidad = st.number_input('Localidad', min_value=None, max_value=None, value=0, step=None,
+                                format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+    municipio = st.number_input('Municipio', min_value=None, max_value=None, value=0, step=None,
+                                format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
-codigos = np.asarray(df_codigos['codigo_postal'])
-index = (np.abs(codigos - codigo)).argmin()
+    df_codigos = df_codigos[(df_codigos['localidad'] == localidad) & (
+        df_codigos['municipio'] == municipio)]
 
-closest_value = codigos[index]
+    codigos = np.asarray(df_codigos['codigo_postal'])
+    index = (np.abs(codigos - codigo)).argmin()
+    closest_value = codigos[index]
 
-st.metric('El código postal válido más cercano es', closest_value, delta=None, delta_color="normal")
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.metric('El código postal válido más cercano es',
+                  closest_value, delta=None, delta_color="normal")
