e9e1188f8c91bcc643e32a1199d26bd6e995d48a
Erika <esanchez1201@alumno.ipn.mx>
2022-02-17 18:34:06 -0600

    App version 1

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title('Encontrar el código postal válido más cercano')
+
+codigo = st.number_input('Código Postal', min_value=None, max_value=None, value=0, step=None,
+                         format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+localidad = st.number_input('Localidad', min_value=None, max_value=None, value=0, step=None,
+                            format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+municipio = st.number_input('Municipio', min_value=None, max_value=None, value=0, step=None,
+                            format=None, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
+
+head_cols = ['codigo_postal', 'estado', 'localidad', 'municipio']
+df_codigos = pd.read_excel('data/catalogo_codigo_postal.xlsx', header=None, names=head_cols)
+
+df_codigos = df_codigos.fillna(-1)
+df_codigos = df_codigos.astype(
+    {'codigo_postal': int, 'localidad': int, 'municipio': int})
+
+df_codigos = df_codigos[(df_codigos['localidad'] == localidad) & (
+    df_codigos['municipio'] == municipio)]
+
+codigos = np.asarray(df_codigos['codigo_postal'])
+index = (np.abs(codigos - codigo)).argmin()
+
+closest_value = codigos[index]
+
+st.metric('El código postal válido más cercano es', closest_value, delta=None, delta_color="normal")
